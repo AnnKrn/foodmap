c0d31bf59708fe350d266d399b41a26d0e723b88
AnnKrn <anakrn.carreon@gmail.com>
2018-01-23 02:02:16 -0600

    busqueda dinamica, revisar sus modales

--- js/vendors/data.js
+++ js/vendors/data.js
@@ -1,8 +1,8 @@
 var restaurants = [ {
-    'name' : 'cochito',
+    'name' : 'Cochinitos',
     'type' : 'Mexicana',
     'location' : 'Calle Izquierda num 20',
-    'picture' : 'assets/images/ñam1.jpg',
+    'picture' : '../assets/images/ñam1.jpg',
     'price' : "100 - 200",
     'description' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     'list': 0
@@ -11,7 +11,7 @@ var restaurants = [ {
     'name' : 'Okana',
     'type' : 'Japonesa',
     'location' : 'Calle Derecha num 40',
-    'picture' : 'assets/images/ñam3.jpg',
+    'picture' : '../assets/images/ñam2.jpg',
     'price' : "200 - 300",
     'description' : 'Necessitatibus quasi dolores maxime magni veniam natus aliquam a quidem?',
     'list': 1
@@ -20,7 +20,7 @@ var restaurants = [ {
     'name' : 'Ñam Ñam',
     'type' : 'Mexicana',
     'location' : 'Calle Izquierda num 20',
-    'picture' : 'assets/images/ñam1.jpg',
+    'picture' : '../assets/images/ñam3.jpg',
     'price' : "100 - 200",
     'description' : 'Necessitatibus quasi dolores maxime magni veniam natus aliquam a quidem?',
     'list': 2    
@@ -29,10 +29,77 @@ var restaurants = [ {
     'name' : 'Osuky',
     'type' : 'Japonesa',
     'location' : 'Calle Derecha num 40',
-    'picture' : 'assets/images/ñam4.jpg',
-    'price' : "200 - 300",
+    'picture' : '../assets/images/ñam4.jpg',
+    'price' : "100 - 200",
     'description' : 'Neque fugiat animi delectus, repellendus deleniti dicta ipsam molestias sed ad eaque.',
     'list': 3
+},
+{
+    'name' : 'MamaMia',
+    'type' : 'Italiana',
+    'location' : 'Calle Derecha num 40',
+    'picture' : '../assets/images/ñam5.jpg',
+    'price' : "200 - 300",
+    'description' : 'Neque fugiat animi delectus, repellendus deleniti dicta ipsam molestias sed ad eaque.',
+    'list': 4
+},
+{
+    'name' : 'Italianito',
+    'type' : 'Italiana',
+    'location' : 'Calle Derecha num 40',
+    'picture' : '../assets/images/ñam6.jpg',
+    'price' : "150 - 300",
+    'description' : 'Neque fugiat animi delectus, repellendus deleniti dicta ipsam molestias sed ad eaque.',
+    'list': 5
+},
+{
+    'name' : 'Pickzha',
+    'type' : 'Thailandesa',
+    'location' : 'Calle Derecha num 40',
+    'picture' : '../assets/images/ñam7.jpg',
+    'price' : "200 - 300",
+    'description' : 'Neque fugiat animi delectus, repellendus deleniti dicta ipsam molestias sed ad eaque.',
+    'list': 6
+},
+{
+    'name' : 'Eraki',
+    'type' : 'Thailandesa',
+    'location' : 'Calle Derecha num 40',
+    'picture' : '../assets/images/ñam8.jpg',
+    'price' : "100 - 250",
+    'description' : 'Neque fugiat animi delectus, repellendus deleniti dicta ipsam molestias sed ad eaque.',
+    'list': 6
+},
+{
+    'name' : 'Thermopolis',
+    'type' : 'Griega',
+    'location' : 'Calle Derecha num 40',
+    'picture' : '../assets/images/ñam9.jpg',
+    'price' : "200 - 300",
+    'description' : 'Neque fugiat animi delectus, repellendus deleniti dicta ipsam molestias sed ad eaque.',
+    'list': 7
+},
+{
+    'name' : 'Mythos',
+    'type' : 'Griega',
+    'location' : 'Calle Derecha num 40',
+    'picture' : '../assets/images/ñam10.jpg',
+    'price' : "100 - 250",
+    'description' : 'Neque fugiat animi delectus, repellendus deleniti dicta ipsam molestias sed ad eaque.',
+    'list': 9
+},
+{
+    'name' : 'Parce',
+    'type' : 'Colombiana',
+    'location' : 'Calle Derecha num 40',
+    'picture' : '../assets/images/ñam11.jpg',
+    'price' : "200 - 300",
+    'description' : 'Neque fugiat animi delectus, repellendus deleniti dicta ipsam molestias sed ad eaque.',
+    'list': 10
 }
+
+
+
+
 ];
 // console.log(restaurants);
---
+++ js/vendors/search.js
@@ -0,0 +1,156 @@
+// traer elementos de html (input), botón. Variables globales
+// var retaurants = [];
+
+var $inputSearch = $('#search');
+
+var $inputButton = $('#search_btn');
+
+var $searchForm = $('#search_form');
+
+function loadPage() {
+    //traer elementos de html. Eventos
+    $inputSearch.keyup(filterCoincidence);
+    //revisar porque no funciona habilidar y deshabilitar boton
+    // $inputSearch.keyup(oneCharacter);
+    $(".photo-restaurant").click(showModal);
+    // $('.rest-info').click(showModal);
+  }
+
+// funciones se desencadenan
+function filterCoincidence () {
+    //obtener el valor del texto ingresado por el usuario
+    //JS vanilla
+    var inputSearchValue = $inputSearch.val().toLowerCase();
+    // console.log(inputSearchValue)
+    //comparar el data si el valor del inut obtenido se encuentra en data, sino pedir otro valor
+    //trim quita espacios en blanco de un string
+    if ($inputSearch.val().trim().length > 0) {
+        // filtrar restaurantes por "type". comprar con key en data
+        var filterRestaurantType = restaurants.filter(function(restaurant) {
+            // console.log(restaurant)
+            // console.log(restaurant.type.toLowerCase().indexOf(inputSearchValue) >= 0);
+            return restaurant.type.toLowerCase().indexOf(inputSearchValue) >= 0;
+        });
+        //prueba .detach(), sino funciona regresar a .empty()
+        $("#food_results").empty();
+        //forEach
+        filterRestaurantType.forEach(function (restaurant) {
+            paintSearchResult(restaurant);
+        });
+        //si es correcta conincidencia pinta los resultados
+    };
+}  
+
+function paintSearchResult (restaurant) {
+    var $foodResults = $("#food_results");
+    // crear elementos en DOM
+        var $rowResults = $("<div />",{ 
+            "class":"row"
+        });
+
+        var $colResults = $("<div />", {
+            "class":"col-12"
+        });
+        // Al crear dinamicamente no respeta los data y no se llama al modal, why?
+        var $divCardResult = $("<div />", {
+            "class": "btn card text-white photo-restaurant"
+        });
+
+        $divCardResult.attr('data-toggle', "modal");
+        $divCardResult.attr('data-target', "#rest-info");
+        $divCardResult.attr('data-list', restaurant.list);
+
+        // var $anchorResult = $("<a />", {
+        //     "class": "card-link card-text"
+        // });
+
+        // var $anchorResultAlong = $("<a />", {
+        //     // "class": "card-link card-text"
+        // });
+
+        var $imgResult = $("<img />", {
+            "class": "card-img costume-size"
+        });
+
+        var $divContentResult = $("<div />", {
+            "class": "card-img-overlay"
+        });
+
+        var $titleContentResult = $("<h5 />", {
+            "class": "card-title"
+        });
+    // agregarles atributos
+        $imgResult.attr("src", restaurant.picture /*"assets/images/ñam5.jpg"*/)
+        // console.log($imgResult)
+    // saignando valores
+        $titleContentResult.text(restaurant.name);
+
+    // acomodarlos (padres e hijos)
+        // $anchorResult.append($imgResult);
+
+        $divContentResult.append($titleContentResult);
+        // $divContentResult.append($anchorResultAlong);
+
+        // $divCardResult.append($anchorResult);
+        // console.log($divCardResult)
+        $divCardResult.append($imgResult);        
+        $divCardResult.append($divContentResult);
+
+        $colResults.append($divCardResult);
+
+        $rowResults.append($colResults);
+
+    // agregar al padre
+    $foodResults.append($rowResults);
+};
+
+// function oneCharacter (e) {
+//     e.preventDefault
+//     if ($(this).val().trim().length > 1 ) {
+//         // prop() permite manejar el valor booleano sin quitar el atributo.
+//         $inputButton.prop("disable", false)
+//         console.log('hola')
+//     } 
+//     else {
+//         $inputButton.prop("disable", true)
+//     };
+// };
+
+// $('.rest-info').modal('hidde');
+
+function showModal () {
+    // valor del elemento seleccionada
+   var $contentModalTitle = $(this).data("list");
+//    console.log($contentModalTitle);
+//    console.log(restaurants[$contentModalTitle].name);
+   //Etiquetas del modal
+   var $restName = $("#rest-name");
+   var $restDescription = $("#rest-description");
+   var $restLocation = $("#rest-location");
+   var $restPrice = $("#rest-price");
+//    var $iguales = {}
+
+    $restName.text(restaurants[$contentModalTitle].name);
+    $restDescription.text(restaurants[$contentModalTitle].description);
+    $restLocation.text(restaurants[$contentModalTitle].location);
+    $restPrice.text(restaurants[$contentModalTitle].price);
+
+    // console.log('')
+    
+    
+   // Obtener valor de data. Intente hacerlo los for pero el objeto que guarda el es el primero, no el que cumple la condicion
+//    Any ideas?
+    // for (var i = 0; i > restaurants.length; i++) {
+    //      if (restaurants[i].name === $contentModalTitle) {
+    //         var $iguales = restaurants[i].name;
+    //          //    $restName.text(restaurants[i].name);
+    //         }
+    //     };  
+        // console.log(restaurants[i].name);
+        // console.log($iguales);
+    // console.log(prueba)
+    
+    // console.log(restaurant[$contentModalTitle])
+};
+
+$(document).ready(loadPage);
